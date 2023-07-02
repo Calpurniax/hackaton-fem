296e572152643cd0c8e6513d6d2c48e8b7680346
NurAhissami <nurahi10@gmail.com>
2023-07-02 22:57:31 +0200

    feat(header): create header component

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import "../../styles/components/header.scss";
+
+export const Header: FC = () => (
+  <header className="c-header">
+    <h1 className="c-header__title">Internet users evolution</h1>
+  </header>
+);
